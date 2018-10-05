e40c8fc685d2c22ea83a98e972c264496e9c51f3
c-alexmala <gpl3App5>
2018-10-05 16:56:07 +0300

    initial commit

---
+++ src/main/java/alexm/spring/boot/tdddemoboot/TddDemoBootApplication.java
@@ -0,0 +1,12 @@
+package alexm.spring.boot.tdddemoboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddDemoBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddDemoBootApplication.class, args);
+	}
+}
